57e64486dd98b56e5f34c303ad183caaf981e89e
João Martins <ajoaosmart@gmail.com>
2020-05-09 21:41:08 +0100

    change update

--- src/app/restrict/expense/list-expenses/list-standard/list-standard.component.ts
+++ src/app/restrict/expense/list-expenses/list-standard/list-standard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { Expense } from '../../Expense';
+import { Router, ActivatedRoute } from '@angular/router';
+import { UpdateService } from 'src/app/shared/update-service/update.service';
 
 @Component({
   selector: 'app-list-standard',
@@ -10,17 +12,34 @@ import { Expense } from '../../Expense';
 export class ListStandardComponent implements OnInit {
 
   @Input() expenseList: Expense[];
+  expense: Expense;
 
-  constructor() { }
+  constructor(private update: UpdateService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
 
-  onEdit(item: any) {
+
+  onEdit(selectedExpense) {
+    this.update.getId(selectedExpense.expenseId);
+    switch(selectedExpense.expenseType) {
+      case 'Despesa corrente fixa':
+        this.router.navigate(['../alterar-despesa/fixa'], { relativeTo: this.route });
+        break;
+        case 'Despesa corrente variável':
+          this.router.navigate(['../alterar-despesa/variavel'], { relativeTo: this.route });
+        break;
+        case 'Despesa não corrente de curta duração':
+          this.router.navigate(['../alterar-despesa/curta'], { relativeTo: this.route });
+        break;
+        case 'Despesa não corrente de longa duração':
+          this.router.navigate(['../alterar-despesa/longa'], { relativeTo: this.route });
+        break;
+    }
     
   }
 
-  onView(item: any) {
+  onView(selectedExpense) {
     
   }
 
